f48d71e4a5f26da084af2f45428822f2e9ade79f
vicky <amosvictory82@gmail.com>
2022-10-15 18:09:12 +0100

    modified

---
+++ buy_now.js
@@ -0,0 +1,25 @@
+// Project on arrow functions
+
+but=document.getElementById("submit")
+const buyNow = () =>{
+
+  const usrInput = document.querySelector('.field-input')
+  if (usrInput.value.length>15){
+    alert("too long")
+  }
+  else{
+    if (usrInput.value===""){
+      alert("Please enter name")
+    }
+    else if(usrInput.value==="Your Name"){
+      alert("Please enter your unique letters")
+
+    }
+    else{
+      alert("Success")
+    }
+  }
+
+
+  }
+but.addEventListener("click", buyNow);
